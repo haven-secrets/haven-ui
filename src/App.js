919212cbfa3d504873fe4440d6c6da95c07d1de7
dan-marino <marinodanielj@hotmail.com>
2020-11-30 22:18:24 -0500

    Displays fake log data. Can filter by project and/or user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import "semantic-ui-css/semantic.min.css";
-import logo from "./logo.svg";
-import "./App.css";
+import Logs from "./components/Logs";
 
 function App() {
   return (
     <div className="App">
-      <button class="ui button">Example SemanticUI button</button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logs />
     </div>
   );
 }
