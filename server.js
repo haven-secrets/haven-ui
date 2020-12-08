7d1bccddb1a1fba9d0157cb7977943b58bb74487
dan-marino <marinodanielj@hotmail.com>
2020-12-08 14:35:11 -0500

    Basic express template working
    Co-authored-by: becco <becco@users.noreply.github.com>
    Co-Authored-By: Adam Isom <adamisom@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const apiRoutes = require("./routes/api");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.post('/api/data', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
+app.use(bodyParser.json());
+app.use("/api", apiRoutes);
+//
+// app.use((err, req, res, next) => {
+//   console.log(err);
+//   next();
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
