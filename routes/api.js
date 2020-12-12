43704196c2e2765aadfe387666bf7dd01afed8f7
dan-marino <marinodanielj@hotmail.com>
2020-12-12 14:30:55 -0500

    Removed unneccessary console logs

--- routes/api.js
+++ routes/api.js
@@ -56,18 +56,15 @@ router.post("/secrets", (req, res, next) => {
 
 router.post("/projects/:name", (req, res, next) => {
   const projectName = req.params.name;
-  console.log("This process will take 30-60 seconds");
   haven.createProject(projectName).then((data) => res.json(data));
 });
 
 router.delete("/projects/:name", (req, res, next) => {
   const projectName = req.params.name;
-  console.log("This process will take 30-60 seconds");
   haven.deleteProject(projectName).then((data) => res.json(data));
 });
 
 router.post("/data", (req, res) => {
-  console.log(req.body);
   res.send(
     `I received your POST request. This is what you sent me: ${JSON.stringify(
       req.body
